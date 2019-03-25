8c5b936927dd5cce24948a02565b4f4b977fe3d0
Sykes2 <43383863+Sykes2@users.noreply.github.com>
2019-03-25 15:58:35 -0700

    added comments for layer class

--- hw4/Layer.java
+++ hw4/Layer.java
@@ -3,25 +3,22 @@ package parallax;
 import java.awt.Graphics;
 import java.awt.Image;
 
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
-
 /**
  *
  * @author Jared
  */
+// This class takes an image that has a width larger/the same as the screen
+// and turns it into an object that can be scrolled acrossed the screen
+// in an endless loop
 public class Layer {
-    private Image image;
+    private Image image;        // Image to be scrolled
     
-    private float screenWidth;
-    private float height;
-    private float x;
-    private float y;
+    private float screenWidth; // width of the screen
+    private float height;      // height of the image
+    private float x;           // location on x axis to display image
+    private float y;           // location on y axis to display image
     
-    private float change;
+    private float change;      // how fast the image scrolls
     
     Layer(float inScreenWidth, Image inImage, float inX, float inY, float inChange){
         image = inImage;
@@ -32,23 +29,22 @@ public class Layer {
         change = inChange;
     }
     
+    // This function calls the draw function depending on where the
+    //  the start of the image is, and takes the part of the image not shown 
+    //  and fills the rest of the layer with it.
+    //  After doing this the function increments x and resets it if x > screenWidth
     void move(Graphics depth){
         if(x < screenWidth){
             draw(depth, 0, x, screenWidth - x, screenWidth);
-            // Head.
+            // draw the rest of the image in the empty space
             draw(depth, x, screenWidth, 0, screenWidth - x);
         } else {
             draw(depth, 0, screenWidth, 0, screenWidth);
         }
-//        if (x > (0 - screenWidth)){
-//            draw(depth, 0, screenWidth + x, (0 - x), screenWidth);
-//            draw(depth, screenWidth + x,  screenWidth - x, 0, screenWidth - x);
-//        } else {
-//            draw(depth, 0, screenWidth, 0, screenWidth);
-//        }
         x = (x + change) % screenWidth;
     }
     
+    // This function draws the image using the x values passed to it
     private void draw(Graphics depth, float startDrawX, float endDrawX, float startImage, float endImage){
         depth.drawImage(image, Math.round(startDrawX), Math.round(y), Math.round(endDrawX), Math.round(y + height), Math.round(startImage), 0, Math.round(endImage), Math.round(height), null);
     }
