a2f8371ce3811fbce21ae831f6fb3af80d6aba26
trodriguez20 <trodriguez@zagmail.gonzaga.edu>
2019-03-24 22:40:41 -0700

    fixed layout of members in parallax and added mouse listeners

--- hw4/Parallax.java
+++ hw4/Parallax.java
@@ -1,113 +1,149 @@
-/*
- * Timothy Rodriguez & Jared Elliott
- */
 package parallax;
 
-import javax.swing.*;
 import java.awt.event.*;
-import java.awt.*;
 import java.awt.Graphics;
 import java.awt.Color;
+import java.awt.PopupMenu;
+import java.util.Random;
 import javax.swing.JPanel;
 import javax.swing.JFrame;
 import javax.swing.Timer;
+import javax.swing.*;
+import java.awt.*;
 import java.io.*;
 import javax.imageio.*;
 
+
 public class Parallax extends JFrame
 {
+    public Parallax()
+    {
+        setTitle("Motion Parallax");
+        setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        fencePanel fp = new fencePanel();
+        add(fp);
+        setSize(2000, 1175);
+        setResizable(false);
+        setVisible(true);
+    }
     
+    class fencePanel extends JPanel implements ActionListener
+    {
+       private Image window;
+       private Image fence;
+       private Image mountains;
+       private Image field;
    
-   public Parallax()
-   {
-      JFrame frame = new JFrame( "Motion Parallax" );
-      frame.setDefaultCloseOperation( JFrame.EXIT_ON_CLOSE );
-      fencePanel fp = new fencePanel(); 
-      frame.add(fp);
-      frame.setSize( 2000, 1175 ); // set frame size
-      frame.setResizable(false);
-      frame.setVisible( true ); // display frame
-   }
-   
-   public static void main( String args[] )
-   {
-	   new Parallax();
-   }
-}
+       private Layer mountLayer;
+       private Layer fieldLayer;
+       private Layer fenceLayer;
+	   
+       private int delay = 100;
+       protected Timer timer;
 
-// class BallPanel
+       private int x = 1800;		// x position
+       private int y = 955;		// y position       
+       private int currentX = 0;
+       private int currentY = 0;
+       
+       
+       Random rand = new Random();
+       Color color1 = new Color(rand.nextInt(255),rand.nextInt(255),rand.nextInt(255));
+      
+       public fencePanel()
+       {
+            try
+            {
+                    window = ImageIO.read(new File("window.png"));
+                    fence = ImageIO.read(new File("fence.png"));
+                    mountains = ImageIO.read(new File("mountains.png"));
+                    field = ImageIO.read(new File("horizon.png"));
+            }
+            catch(IOException e)
+            {
+                    System.out.println("Error opening image files");
+            }
+            mountLayer = new Layer(2000, mountains, 0, 0, 5); 
+            fieldLayer = new Layer(2000, field, 0, 650, 10);
+            fenceLayer = new Layer(2000, fence, 0, 950, 20);
 
-class fencePanel extends JPanel implements ActionListener
-{
-   private Image window;
-   private Image fence;
-   
-   private Image mountains;
-   private Image field;
-   private Layer mountLayer;
-   private Layer fieldLayer;
-   private Layer fenceLayer;
-   
-   private int delay = 10;
-   protected Timer timer;
+            timer = new Timer(delay, this);
+            timer.start();		// start the timer
 
-   private int x = 1800;		// x position
-   private int y = 955;		// y position
-   private int halfFence = 15;	// ball radius
+            addMouseListener(new MyMouseListener());
+       }
 
-   private int dx = 2;		// increment amount (x coord)
-   //private int dy = 2;		// increment amount (y coord)
+       public void actionPerformed(ActionEvent e)
+       // will run when the timer fires
+       {
+            repaint();
+       }
 
-   public fencePanel()
-   {
-        try
-      {
-          window = ImageIO.read(new File("window.png"));
-          fence = ImageIO.read(new File("fence.png"));
-          mountains = ImageIO.read(new File("mountains.png"));
-          field = ImageIO.read(new File("horizon.png"));
-      }
-      catch(IOException e)
-      {
-          System.out.println("Error opening image files");
-      }
-        
-      mountLayer = new Layer(2000, mountains, 0, 0, 5); 
-      fieldLayer = new Layer(2000, field, 0, 650, 10);
-      fenceLayer = new Layer(2000, fence, 0, 950, 20);
-      timer = new Timer(delay, this);
-	timer.start();		// start the timer
-   }
+       public void paintComponent( Graphics g )
+       {
+            super.paintComponent(g); // call superclass's paintComponent 
+            g.setColor(color1);
+            g.fillRect(0, 0, 2000, 1125);
+            mountLayer.move(g);
+            fieldLayer.move(g);
+            fenceLayer.move(g);
+            g.drawImage(window, 0, 0, null);
+            
+           
+/*
+            // creates a mountain
+            g.setColor(color1);
+            g.fillPolygon(aValues, bValues, 3);
 
-   public void actionPerformed(ActionEvent e)
-   // will run when the timer fires
-   {
-	repaint();
-   }
+            // creates another mountain
+            g.setColor(color2);
+            g.fillPolygon(xValues, yValues, 3);
+*/
+     
 
-   // draw rectangles and arcs
-   public void paintComponent( Graphics g )
-   {
-      super.paintComponent( g ); // call superclass's paintComponent 
-	//g.setColor(Color.red);
-        
-        mountLayer.move(g);
-        fieldLayer.move(g);
-        fenceLayer.move(g);
-        
-	// check for boundaries
-	//if (x < halfFence)			dx = Math.abs(dx);
-	//if (x > getWidth() - halfFence)	dx = -Math.abs(dx);
-	//if (y < radius)			dy = Math.abs(dy);
-	//if (y > getHeight() - radius)	dy = -Math.abs(dy);
+       }
+       
+       private class MyMouseListener extends MouseAdapter
+       {
+           /*
+            public void mousePressed(MouseEvent e)
+            {
+               setBackground(Color.GRAY);
+               repaint();
+               
+            }        
+            public void mouseClicked(MouseEvent e)
+            {
+                color1 = new Color(rand.nextInt(255),rand.nextInt(255),rand.nextInt(255));
+                color2 = new Color(rand.nextInt(255),rand.nextInt(255),rand.nextInt(255));
+                repaint();
+              
+            }
+            public void mouseReleased(MouseEvent e)
+            {
+                setBackground(Color.ORANGE);
+                repaint();
+            }
+			*/
+            public void mouseEntered(MouseEvent e)
+            {
+		color1 = new Color(153, 217, 234);
+                //setBackground(Color.cyan);
+                repaint();         
+                    
+            }
+            public void mouseExited(MouseEvent e)
+            {
+                color1 = new Color(57, 86, 125);
+                //setBackground(Color.blue);
+                repaint();
+            }
+        }
+    }
 
-	// adjust ball position
-	//x -= dx;
-	//y += dy;
-	//g.drawImage(fence, x-halfFence, y, null);
-        g.drawImage(window, 0, 0, null);
-	//g.fillOval(x - radius, y - radius, radius*2, radius*2);
-   }
-  
-
+    public static void main(String[] args) 
+    {
+        new Parallax();
+     
+    }
 }
