358bf10a1d75c90b9993d566006dc8001427ef0e
trodriguez20 <trodriguez@zagmail.gonzaga.edu>
2019-04-16 10:16:15 -0700

    basic stuff added

--- tanks/Tanks.java
+++ tanks/Tanks.java
@@ -1,20 +1,61 @@
 /*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
+ * Jared Elliott & Timothy Rodriguez
+ * FINAL PROJECT
+ * 4/16/19
  */
 package tanks;
 
-/**
- *
- * @author m14wo
- */
-public class Tanks {
+import java.awt.event.*;
+import java.awt.Graphics;
+import java.awt.Color;
+import java.awt.PopupMenu;
+import java.util.Random;
+import javax.swing.JPanel;
+import javax.swing.JFrame;
+import javax.swing.Timer;
+import javax.swing.*;
+import java.awt.*;
+import java.io.*;
+import java.awt.image.BufferedImage;
+import static java.lang.Math.max;
+import static java.lang.Math.min;
+import javax.imageio.*;
 
-    /**
-     * @param args the command line arguments
-     */
-    public static void main(String[] args) {
+public class Tanks extends JFrame
+{
+    public Tanks()
+    {
+        setTitle("Tanks");
+        // set the frame to exit on close
+        setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        //create a new panel of class fencePanel called fp
+        titlePanel tp = new titlePanel();
+        add(tp);
+       //set size of the JFrame windo to 2000x1175 pixels.
+        setSize(1600, 900);
+        setResizable(false);
+        setVisible(true);
+    }
+    
+    class titlePanel extends JPanel
+    {
+       private Image title;
+       private Image tank;
+       private Image mascot;
+       private int delay = 100;
+       protected Timer timer;
+       
+       public titlePanel()
+       {
+           
+       }
+       }
+       
+    }
+
+
+    public static void main(String[] args)
+    {
         // TODO code application logic here
     }
     
